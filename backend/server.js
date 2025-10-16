e8afc72e8253308c920a5b36c6bc409b4ed1b141
ananya-dhiman <ananya.dhiman@somaiya.edu>
2025-10-16 15:12:31 +0530

    Initial commit

---
+++ backend/server.js
@@ -0,0 +1,22 @@
+    require('dotenv').config(); 
+
+    const express = require('express');
+    const mongoose = require('mongoose');
+    const app = express();
+    const PORT = process.env.PORT || 3000;
+
+    mongoose.connect(process.env.MONGO_URI, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+    })
+    .then(() => console.log('MongoDB connected successfully!'))
+    .catch(err => console.error('MongoDB connection error:', err));
+
+
+    app.get('/', (req, res) => {
+        res.send('Backend is running!');
+    });
+
+    app.listen(PORT, () => {
+        console.log(`Server running on port ${PORT}`);
+    });
