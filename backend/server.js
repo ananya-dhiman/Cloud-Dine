4651725575c70fa89c85af664992740991681004
Ananya Dhiman <ananya.dhiman@somaiya.edu>
2025-10-30 16:13:10 +0530

    Revert "Added FireBase Auth"
    This reverts commit 030d918e48385d49aa7d0bea843aaa77dc1be196.

--- backend/server.js
+++ backend/server.js
@@ -15,7 +15,10 @@
 
     app.use(express.json());
 
-    mongoose.connect(process.env.MONGO_URI)
+    mongoose.connect(process.env.MONGO_URI, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+    })
     .then(() => console.log('MongoDB connected successfully!'))
     .catch(err => console.error('MongoDB connection error:', err));
 
@@ -27,7 +30,7 @@
     app.use('/api/orders', orderRoutes);
     app.use('/api/reviews', reviewRoutes);  
     app.use('/api/kitchens', kitchenRoutes);
-     app.use('/api/users', userRoutes);
+    // app.use('/api/users', userRoutes);
     app.use('/api/menus', menuRoutes);
 
     app.listen(PORT, () => {
