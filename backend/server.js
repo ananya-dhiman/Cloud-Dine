e2aa8ed1a2fba484de219f7327589bc711bb84d1
Ananya Dhiman <ananya.dhiman@somaiya.edu>
2025-11-10 21:52:50 +0530

    Added README.md

--- backend/server.js
+++ backend/server.js
@@ -1,53 +1,53 @@
-    import dotenv from 'dotenv';
-    dotenv.config();
+      import dotenv from 'dotenv';
+      dotenv.config();
 
-    import express from 'express';
-    import path from 'path';
-    import mongoose from 'mongoose';
-    const app = express();
-    const PORT = process.env.PORT || 3001; 
-    import orderRoutes from './routes/orderRoutes.js';
-    import reviewRoutes from './routes/reviewRoutes.js';
-    import kitchenRoutes from './routes/kitchenRoutes.js';
-    import userRoutes from './routes/userRoutes.js';
-    import menuRoutes from './routes/menuRoutes.js';
-    import cors from "cors";
+      import express from 'express';
+      import path from 'path';
+      import mongoose from 'mongoose';
+      const app = express();
+      const PORT = process.env.PORT || 3001; 
+      import orderRoutes from './routes/orderRoutes.js';
+      import reviewRoutes from './routes/reviewRoutes.js';
+      import kitchenRoutes from './routes/kitchenRoutes.js';
+      import userRoutes from './routes/userRoutes.js';
+      import menuRoutes from './routes/menuRoutes.js';
+      import cors from "cors";
 
-    const allowedOrigins = [
-                  // final domain
-  "https://cloud-dine-x31v.vercel.app",            // backend domain (self)
-  "https://cloud-dine-h0w41a840-dhimanananya1901-6638s-projects.vercel.app",
-  "https://cloud-dine.vercel.app"
-];
+      const allowedOrigins = [
+                    // final domain
+    "https://cloud-dine-x31v.vercel.app",            // backend domain (self)
+    "https://cloud-dine-h0w41a840-dhimanananya1901-6638s-projects.vercel.app",
+    "https://cloud-dine.vercel.app"
+  ];
 
-    app.use(express.json());
-    app.use(
-    cors({
-       origin: (origin, callback) => {
-      if (!origin || allowedOrigins.includes(origin)) {
-        callback(null, true);
-      } else {
-        callback(new Error("Not allowed by CORS"));
-      }
-    },
-        credentials: true, 
-    })
-    );
-    mongoose.connect(process.env.MONGO_URI)
-    .then(() => console.log('MongoDB connected successfully!'))
-    .catch(err => console.error('MongoDB connection error:', err));
+      app.use(express.json());
+      app.use(
+      cors({
+        origin: (origin, callback) => {
+        if (!origin || allowedOrigins.includes(origin)) {
+          callback(null, true);
+        } else {
+          callback(new Error("Not allowed by CORS"));
+        }
+      },
+          credentials: true, 
+      })
+      );
+      mongoose.connect(process.env.MONGO_URI)
+      .then(() => console.log('MongoDB connected successfully!'))
+      .catch(err => console.error('MongoDB connection error:', err));
 
 
-    app.get('/', (req, res) => {
-        res.send('Backend is running!');
-    });
+      app.get('/', (req, res) => {
+          res.send('Backend is running!');
+      });
 
-    app.use('/api/orders', orderRoutes);
-    app.use('/api/reviews', reviewRoutes);  
-    app.use('/api/kitchens', kitchenRoutes);
-    app.use('/api/users', userRoutes);
-    app.use('/api/menus', menuRoutes);
+      app.use('/api/orders', orderRoutes);
+      app.use('/api/reviews', reviewRoutes);  
+      app.use('/api/kitchens', kitchenRoutes);
+      app.use('/api/users', userRoutes);
+      app.use('/api/menus', menuRoutes);
 
-    app.listen(PORT, () => {
-        console.log(`Server running on port ${PORT}`);
-    });
+      app.listen(PORT, () => {
+          console.log(`Server running on port ${PORT}`);
+      });
--- frontend/src/Common/auth-form.jsx
+++ frontend/src/Common/auth-form.jsx
@@ -173,7 +173,7 @@
 //   );
 // }
 import React, { useState } from "react";
-import { motion } from "framer-motion";
+
 import axios from "axios";
 import {
   signInWithEmailAndPassword,
